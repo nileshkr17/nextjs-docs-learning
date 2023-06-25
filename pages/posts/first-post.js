5c1dfb2eaf0346f2cf294121e877f8fc2f24adec
Nilesh <ece.20becd76@silicon.ac.in>
2023-06-26 01:36:09 +0530

    [added-readme]

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -5,11 +5,10 @@ import Layout  from '../../components/layout';
 
 export default function FirstPost(){
     return(
-        <Layout>
+        <Layout >
         <Head>
             <title>First Post</title>
         </Head>
-
         <h1>First Post</h1>
         <h2>
             <Link href="/" >Back to Home </Link>
